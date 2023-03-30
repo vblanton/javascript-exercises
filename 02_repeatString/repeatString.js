bd8a5711c118ff5e24810c50815d4c52a17adbbb
Vlad <37779807+vblanton@users.noreply.github.com>
2023-03-29 20:42:03 -0400

    completed first two Jest exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {return "ERROR"};
+    let newString = "";
+    for (x = 1; x <= num; x++) {
+        newString = newString += string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
