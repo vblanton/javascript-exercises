34bfe8e92f1e4d82fa44510ab6cca8ee56e2efa7
Vlad <37779807+vblanton@users.noreply.github.com>
2023-04-03 20:44:16 -0400

    finished test 06

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!args.includes(arr[i])) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+if (num1 < 0 || num2 < 0) {
+    return 'ERROR';
+}
+if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return 'ERROR';
+}
+let sum = 0;
+if (num1 < num2) {
+    for (let i = num1; i <= num2; i++) {
+        sum += i;
+    }
+} else {
+    for (let i = num2; i <= num1; i++) {
+        sum += i;
+    }
+}
+console.log(sum);
+return sum;
 };
 
 // Do not edit below this line
