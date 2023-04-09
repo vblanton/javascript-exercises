aca83504d008638d320ced0405977a4282561553
Vlad Blanton <37779807+vblanton@users.noreply.github.com>
2023-04-09 16:07:46 -0400

    completed Jest test 10 - Fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {       
+    
+    if (num < 0) {
+        return "OOPS";
+    } else if (num == 0) {
+        return 0;
+    } else if (num == 1) {
+        return 1;
+    } else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
+    
 };
 
 // Do not edit below this line
