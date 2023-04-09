db6fc59544d08dcc4d1fc8bb640043ac979c60e7
Vlad Blanton <37779807+vblanton@users.noreply.github.com>
2023-04-09 19:10:10 -0400

    completed last test number 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {    
+    let oldest = arr[0];
+    if (oldest.yearOfDeath === undefined) {
+        oldest.yearOfDeath = new Date().getFullYear();
+    }
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i].yearOfDeath === undefined) {
+            arr[i].yearOfDeath = new Date().getFullYear();
+        }
+        if (oldest.yearOfDeath - oldest.yearOfBirth < arr[i].yearOfDeath - arr[i].yearOfBirth) {
+            oldest = arr[i];
+        }
+    }
+    return oldest;
 
 };
 
